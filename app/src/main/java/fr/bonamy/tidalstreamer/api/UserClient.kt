a63c000c9accfaa65c95230ad71d009e6b3c6f7e
Nicolas Bonamy <nicolas@bonamy.fr>
2025-01-30 17:51:28 -0600

    mixes order fix

--- app/src/main/java/fr/bonamy/tidalstreamer/api/UserClient.kt
+++ app/src/main/java/fr/bonamy/tidalstreamer/api/UserClient.kt
@@ -8,117 +8,129 @@ import fr.bonamy.tidalstreamer.models.Track
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class UserClient: ApiClient() {
+class UserClient : ApiClient() {
 
-	suspend fun fetchShortcuts(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getShortcuts()
-			fetchResponse(response)
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchShortcuts(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getShortcuts()
+      fetchResponse(response)
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchMixes(): ApiResult<List<Mix>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getMixes()
-			fetchResponse(response)
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchMixes(): ApiResult<List<Mix>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getMixes()
+      if (response.isSuccessful && response.body()!!.status == "ok") {
+        ApiResult.Success(response.body()!!.result!!.sortedWith { a, _ ->
+          when (a.type) {
+            "DISCOVERY_MIX", "NEW_RELEASE_MIX" -> -1
+            else -> 1
+          }
+        })
+      } else {
+        ApiResult.Error(Throwable("Error: ${response.code()}"))
+      }
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchFavoriteAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getFavoriteAlbums()
-			if (response.isSuccessful && response.body()!!.status == "ok") {
-				ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.created }.map { it.item!! })
-			} else {
-				ApiResult.Error(Throwable("Error: ${response.code()}"))
-			}
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchFavoriteAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getFavoriteAlbums()
+      if (response.isSuccessful && response.body()!!.status == "ok") {
+        ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.created }
+          .map { it.item!! })
+      } else {
+        ApiResult.Error(Throwable("Error: ${response.code()}"))
+      }
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchFavoriteArtists(): ApiResult<List<Artist>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getFavoriteArtists()
-			if (response.isSuccessful && response.body()!!.status == "ok") {
-				ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.created }.map { it.item!! })
-			} else {
-				ApiResult.Error(Throwable("Error: ${response.code()}"))
-			}
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchFavoriteArtists(): ApiResult<List<Artist>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getFavoriteArtists()
+      if (response.isSuccessful && response.body()!!.status == "ok") {
+        ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.created }
+          .map { it.item!! })
+      } else {
+        ApiResult.Error(Throwable("Error: ${response.code()}"))
+      }
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchFavoriteTracks(): ApiResult<List<Track>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getFavoriteTracks()
-			if (response.isSuccessful && response.body()!!.status == "ok") {
-				ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.created }.map { it.item!! })
-			} else {
-				ApiResult.Error(Throwable("Error: ${response.code()}"))
-			}
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchFavoriteTracks(): ApiResult<List<Track>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getFavoriteTracks()
+      if (response.isSuccessful && response.body()!!.status == "ok") {
+        ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.created }
+          .map { it.item!! })
+      } else {
+        ApiResult.Error(Throwable("Error: ${response.code()}"))
+      }
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchPlaylists(): ApiResult<List<Playlist>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getPlaylists()
-			if (response.isSuccessful && response.body()!!.status == "ok") {
-				ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.lastUpdated })
-			} else {
-				ApiResult.Error(Throwable("Error: ${response.code()}"))
-			}
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchPlaylists(): ApiResult<List<Playlist>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getPlaylists()
+      if (response.isSuccessful && response.body()!!.status == "ok") {
+        ApiResult.Success(response.body()!!.result!!.items!!.sortedByDescending { it.lastUpdated })
+      } else {
+        ApiResult.Error(Throwable("Error: ${response.code()}"))
+      }
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchNewAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getNewAlbums()
-			fetchResponse(response)
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchNewAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getNewAlbums()
+      fetchResponse(response)
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchRecentAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getRecentAlbums()
-			fetchResponse(response)
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchRecentAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getRecentAlbums()
+      fetchResponse(response)
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchRecentArtists(): ApiResult<List<Artist>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getRecentArtists()
-			fetchResponse(response)
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchRecentArtists(): ApiResult<List<Artist>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getRecentArtists()
+      fetchResponse(response)
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	suspend fun fetchRecommendedAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
-		try {
-			val response = apiService.getRecommendedAlbums()
-			fetchResponse(response)
-		} catch (e: Exception) {
-			ApiResult.Error(e)
-		}
-	}
+  suspend fun fetchRecommendedAlbums(): ApiResult<List<Album>> = withContext(Dispatchers.IO) {
+    try {
+      val response = apiService.getRecommendedAlbums()
+      fetchResponse(response)
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
+  }
 
-	private val apiService: UserService by lazy {
-		ApiRetrofitClient.instance.create(UserService::class.java)
-	}
+  private val apiService: UserService by lazy {
+    ApiRetrofitClient.instance.create(UserService::class.java)
+  }
 
 }
 
