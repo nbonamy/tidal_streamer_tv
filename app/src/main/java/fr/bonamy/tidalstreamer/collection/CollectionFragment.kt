3546ea50de7fe4681b948e5363561338c3ddf39d
Nicolas Bonamy <nicolas@bonamy.fr>
2025-01-30 08:31:23 -0600

    collection details

--- app/src/main/java/fr/bonamy/tidalstreamer/collection/CollectionFragment.kt
+++ app/src/main/java/fr/bonamy/tidalstreamer/collection/CollectionFragment.kt
@@ -28,6 +28,7 @@ import com.bumptech.glide.request.transition.Transition
 import fr.bonamy.tidalstreamer.MainActivity
 import fr.bonamy.tidalstreamer.R
 import fr.bonamy.tidalstreamer.api.ApiResult
+import fr.bonamy.tidalstreamer.api.EnqueuePosition
 import fr.bonamy.tidalstreamer.api.MetadataClient
 import fr.bonamy.tidalstreamer.api.StreamingClient
 import fr.bonamy.tidalstreamer.artist.ArtistActivity
@@ -46,294 +47,290 @@ import kotlinx.coroutines.launch
  */
 class CollectionFragment : DetailsSupportFragment(), PaletteAsyncListener, OnTrackClickListener {
 
-	private var mCollection: Collection? = null
-	private lateinit var mBackgroundManager: BackgroundManager
-	private lateinit var mDetailsBackground: DetailsSupportFragmentBackgroundController
-	private lateinit var mDetailsPresenter: FullWidthDetailsOverviewRowPresenter
-	private lateinit var mPresenterSelector: ClassPresenterSelector
-	private lateinit var mAdapter: ArrayObjectAdapter
+  private var mCollection: Collection? = null
+  private lateinit var mBackgroundManager: BackgroundManager
+  private lateinit var mDetailsBackground: DetailsSupportFragmentBackgroundController
+  private lateinit var mDetailsPresenter: FullWidthDetailsOverviewRowPresenter
+  private lateinit var mPresenterSelector: ClassPresenterSelector
+  private lateinit var mAdapter: ArrayObjectAdapter
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		Log.d(TAG, "onCreate DetailsFragment")
-		super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    Log.d(TAG, "onCreate DetailsFragment")
+    super.onCreate(savedInstanceState)
 
-		mDetailsBackground = DetailsSupportFragmentBackgroundController(this)
-		mBackgroundManager = BackgroundManager.getInstance(requireActivity())
-		mBackgroundManager.attach(requireActivity().window)
+    mDetailsBackground = DetailsSupportFragmentBackgroundController(this)
+    mBackgroundManager = BackgroundManager.getInstance(requireActivity())
+    mBackgroundManager.attach(requireActivity().window)
 
-		mCollection =
-			requireActivity().intent.getSerializableExtra(CollectionActivity.COLLECTION) as Collection
-		if (mCollection == null) {
-			val intent = Intent(requireContext(), MainActivity::class.java)
-			startActivity(intent)
-		}
+    mCollection =
+      requireActivity().intent.getSerializableExtra(CollectionActivity.COLLECTION) as Collection
+    if (mCollection == null) {
+      val intent = Intent(requireContext(), MainActivity::class.java)
+      startActivity(intent)
+    }
 
-		mPresenterSelector = ClassPresenterSelector()
-		mAdapter = ArrayObjectAdapter(mPresenterSelector)
-		setupDetailsOverviewRow()
-		setupDetailsOverviewRowPresenter()
-		adapter = mAdapter
+    mPresenterSelector = ClassPresenterSelector()
+    mAdapter = ArrayObjectAdapter(mPresenterSelector)
+    setupDetailsOverviewRow()
+    setupDetailsOverviewRowPresenter()
+    adapter = mAdapter
 
-		if (mCollection!!.tracks == null) {
+    if (mCollection!!.tracks == null) {
 
-			lifecycleScope.launch {
-				val apiClient = MetadataClient()
-				when (mCollection) {
-					is Album -> {
-						when (val result = apiClient.fetchAlbumTracks((mCollection as Album).id!!)) {
-							is ApiResult.Success -> {
-								mCollection!!.tracks = result.data
-								mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
-							}
+      lifecycleScope.launch {
+        val apiClient = MetadataClient()
+        when (mCollection) {
+          is Album -> {
+            when (val result = apiClient.fetchAlbumTracks((mCollection as Album).id!!)) {
+              is ApiResult.Success -> {
+                mCollection!!.tracks = result.data
+                mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
+              }
 
-							is ApiResult.Error -> {
-								// Handle the error here
-								Log.e(TAG, "Error fetching album tracks: ${result.exception}")
-							}
-						}
-					}
+              is ApiResult.Error -> {
+                // Handle the error here
+                Log.e(TAG, "Error fetching album tracks: ${result.exception}")
+              }
+            }
+          }
 
-					is Mix -> {
-						when (val result = apiClient.fetchMixTracks((mCollection as Mix).id!!)) {
-							is ApiResult.Success -> {
-								mCollection!!.tracks = result.data
-								mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
-							}
+          is Mix -> {
+            when (val result = apiClient.fetchMixTracks((mCollection as Mix).id!!)) {
+              is ApiResult.Success -> {
+                mCollection!!.tracks = result.data
+                mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
+              }
 
-							is ApiResult.Error -> {
-								// Handle the error here
-								Log.e(TAG, "Error fetching mix tracks: ${result.exception}")
-							}
-						}
-					}
+              is ApiResult.Error -> {
+                // Handle the error here
+                Log.e(TAG, "Error fetching mix tracks: ${result.exception}")
+              }
+            }
+          }
 
-					is Playlist -> {
-						when (val result = apiClient.fetchPlaylistTracks((mCollection as Playlist).uuid!!)) {
-							is ApiResult.Success -> {
-								mCollection!!.tracks = result.data
-								mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
-							}
+          is Playlist -> {
+            when (val result = apiClient.fetchPlaylistTracks((mCollection as Playlist).uuid!!)) {
+              is ApiResult.Success -> {
+                mCollection!!.tracks = result.data
+                mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
+              }
 
-							is ApiResult.Error -> {
-								// Handle the error here
-								Log.e(TAG, "Error fetching playlist tracks: ${result.exception}")
-							}
-						}
-					}
-				}
-			}
+              is ApiResult.Error -> {
+                // Handle the error here
+                Log.e(TAG, "Error fetching playlist tracks: ${result.exception}")
+              }
+            }
+          }
+        }
+      }
 
-		}
+    }
 
-	}
+  }
 
-	override fun onResume() {
-		super.onResume()
-		initializeBackground(mCollection)
-	}
+  override fun onResume() {
+    super.onResume()
+    initializeBackground(mCollection)
+  }
 
-	private fun initializeBackground(collection: Collection?) {
-		//mDetailsBackground.enableParallax()
-		Glide.with(requireContext())
-			.asBitmap()
-			.centerCrop()
-			.error(R.drawable.default_background)
-			.load(collection?.largeImageUrl())
-			.into(object : CustomTarget<Bitmap>() {
+  private fun initializeBackground(collection: Collection?) {
+    //mDetailsBackground.enableParallax()
+    Glide.with(requireContext())
+      .asBitmap()
+      .centerCrop()
+      .error(R.drawable.default_background)
+      .load(collection?.largeImageUrl())
+      .into(object : CustomTarget<Bitmap>() {
 
-				override fun onResourceReady(
-					bitmap: Bitmap,
-					transition: Transition<in Bitmap?>?
-				) {
-					mBackgroundManager.setBitmap(bitmap)
-				}
+        override fun onResourceReady(
+          bitmap: Bitmap,
+          transition: Transition<in Bitmap?>?
+        ) {
+          mBackgroundManager.setBitmap(bitmap)
+        }
 
-				override fun onLoadCleared(placeholder: Drawable?) {
+        override fun onLoadCleared(placeholder: Drawable?) {
 
-				}
-			})
+        }
+      })
 
-	}
+  }
 
-	private fun setupDetailsOverviewRow() {
-		Log.d(TAG, "doInBackground: " + mCollection?.toString())
-		val row = DetailsOverviewRow(mCollection)
-		row.imageDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.default_background)
-		val width = convertDpToPixel(requireContext(), DETAIL_THUMB_WIDTH)
-		val height = convertDpToPixel(requireContext(), DETAIL_THUMB_HEIGHT)
-		Glide.with(requireContext())
-			.load(mCollection?.imageUrl())
-			.centerCrop()
-			.error(R.drawable.album)
-			.into(object : CustomTarget<Drawable>(width, height) {
-				override fun onResourceReady(
-					drawable: Drawable,
-					transition: Transition<in Drawable?>?
-				) {
-					Log.d(TAG, "details overview card image url ready: $drawable")
-					row.imageDrawable = drawable
-					mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
-				}
+  private fun setupDetailsOverviewRow() {
+    Log.d(TAG, "doInBackground: " + mCollection?.toString())
+    val row = DetailsOverviewRow(mCollection)
+    row.imageDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.default_background)
+    val width = convertDpToPixel(requireContext(), DETAIL_THUMB_WIDTH)
+    val height = convertDpToPixel(requireContext(), DETAIL_THUMB_HEIGHT)
+    Glide.with(requireContext())
+      .load(mCollection?.imageUrl())
+      .centerCrop()
+      .error(R.drawable.album)
+      .into(object : CustomTarget<Drawable>(width, height) {
+        override fun onResourceReady(drawable: Drawable, transition: Transition<in Drawable?>?) {
+          Log.d(TAG, "details overview card image url ready: $drawable")
+          row.imageDrawable = drawable
+          mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
+        }
 
-				override fun onLoadCleared(placeholder: Drawable?) {
+        override fun onLoadCleared(placeholder: Drawable?) {
 
-				}
-			})
+        }
+      })
 
-		val actionAdapter = ArrayObjectAdapter()
+    val actionAdapter = ArrayObjectAdapter()
 
-		actionAdapter.add(
-			Action(
-				ACTION_PLAY_NOW,
-				resources.getString(R.string.play_now),
-			)
-		)
-//		actionAdapter.add(
-//			Action(
-//				ACTION_PLAY_NEXT,
-//				resources.getString(R.string.play_next),
-//			)
-//		)
-//		actionAdapter.add(
-//			Action(
-//				ACTION_QUEUE,
-//				resources.getString(R.string.play_after),
-//			)
-//		)
+    actionAdapter.add(Action(ACTION_PLAY_NOW, resources.getString(R.string.play_now)))
+    actionAdapter.add(Action(ACTION_PLAY_NEXT, resources.getString(R.string.play_next)))
+    actionAdapter.add(Action(ACTION_QUEUE, resources.getString(R.string.play_after)))
 
-		if (mCollection is Album && (mCollection as Album).mainArtist() != null) {
-			actionAdapter.add(
-				Action(
-					ACTION_GO_TO_ARTIST,
-					resources.getString(R.string.go_to_artist),
-				)
-			)
-		}
+    if (mCollection is Album && (mCollection as Album).mainArtist() != null) {
+      actionAdapter.add(Action(ACTION_GO_TO_ARTIST, resources.getString(R.string.go_to_artist)))
+    }
 
-		row.actionsAdapter = actionAdapter
+    row.actionsAdapter = actionAdapter
+    mAdapter.add(row)
+  }
 
-		mAdapter.add(row)
-	}
+  private fun setupDetailsOverviewRowPresenter() {
 
-	private fun setupDetailsOverviewRowPresenter() {
+    // Set detail background.
+    mDetailsPresenter = FullWidthDetailsOverviewRowPresenter(DetailsPresenter(mCollection!!, this))
+    mDetailsPresenter.backgroundColor = ColorUtils.setAlphaComponent(ContextCompat.getColor(requireContext(), R.color.details_background), ALPHA_VALUE)
+    mDetailsPresenter.actionsBackgroundColor = ContextCompat.getColor(requireContext(), R.color.details_background)
 
-		// Set detail background.
-		mDetailsPresenter =
-			FullWidthDetailsOverviewRowPresenter(DetailsPresenter(mCollection!!, this))
-		mDetailsPresenter.backgroundColor =
-			ColorUtils.setAlphaComponent(ContextCompat.getColor(requireContext(), R.color.details_background), ALPHA_VALUE)
-		mDetailsPresenter.actionsBackgroundColor =
-			ContextCompat.getColor(requireContext(), R.color.details_background)
+    // Hook up transition element.
+    val sharedElementHelper = FullWidthDetailsOverviewSharedElementHelper()
+    sharedElementHelper.setSharedElementEnterTransition(activity, CollectionActivity.SHARED_ELEMENT_NAME)
+    mDetailsPresenter.setListener(sharedElementHelper)
+    mDetailsPresenter.isParticipatingEntranceTransition = true
 
-		// Hook up transition element.
-		val sharedElementHelper = FullWidthDetailsOverviewSharedElementHelper()
-		sharedElementHelper.setSharedElementEnterTransition(
-			activity, CollectionActivity.SHARED_ELEMENT_NAME
-		)
-		mDetailsPresenter.setListener(sharedElementHelper)
-		mDetailsPresenter.isParticipatingEntranceTransition = true
+    mDetailsPresenter.onActionClickedListener = OnActionClickedListener { action ->
 
-		mDetailsPresenter.onActionClickedListener = OnActionClickedListener { action ->
+      if (action.id == ACTION_PLAY_NOW) {
+        playCollection()
+        return@OnActionClickedListener
+      }
 
-			if (action.id == ACTION_PLAY_NOW) {
-				playCollection()
-				return@OnActionClickedListener
-			}
+      if (action.id == ACTION_PLAY_NEXT || action.id == ACTION_QUEUE) {
+        val position = if (action.id == ACTION_PLAY_NEXT) EnqueuePosition.NEXT else EnqueuePosition.END
+        queueCollection(position)
+        return@OnActionClickedListener
+      }
 
-			if (action.id == ACTION_GO_TO_ARTIST) {
-				Intent(requireContext(), ArtistActivity::class.java).apply {
-					putExtra(ArtistActivity.ARTIST, (mCollection!! as Album).mainArtist()!!)
-					startActivity(this)
-				}
-				return@OnActionClickedListener
-			}
+      if (action.id == ACTION_GO_TO_ARTIST) {
+        Intent(requireContext(), ArtistActivity::class.java).apply {
+          putExtra(ArtistActivity.ARTIST, (mCollection!! as Album).mainArtist()!!)
+          startActivity(this)
+        }
+        return@OnActionClickedListener
+      }
 
-			// default for now
-			Toast.makeText(requireContext(), action.toString(), Toast.LENGTH_SHORT).show()
+      // default for now
+      Toast.makeText(requireContext(), action.toString(), Toast.LENGTH_SHORT).show()
 
-		}
+    }
 
-		mPresenterSelector.addClassPresenter(DetailsOverviewRow::class.java, mDetailsPresenter)
-	}
+    mPresenterSelector.addClassPresenter(DetailsOverviewRow::class.java, mDetailsPresenter)
+  }
 
-	private fun convertDpToPixel(context: Context, dp: Int): Int {
-		val density = context.applicationContext.resources.displayMetrics.density
-		return Math.round(dp.toFloat() * density)
-	}
+  private fun convertDpToPixel(context: Context, dp: Int): Int {
+    val density = context.applicationContext.resources.displayMetrics.density
+    return Math.round(dp.toFloat() * density)
+  }
 
-	override fun onTrackClick(track: Track) {
-		val index = mCollection!!.tracks!!.indexOf(track)
-		playCollection(index)
-	}
+  override fun onTrackClick(track: Track) {
+    val index = mCollection!!.tracks!!.indexOf(track)
+    playCollection(index)
+  }
 
-	override fun onTrackLongClick(track: Track) {
-		TrackLongClickListener(requireActivity()).onTrackLongClicked(track, null)
-	}
+  override fun onTrackLongClick(track: Track) {
+    TrackLongClickListener(requireActivity()).onTrackLongClicked(track, null)
+  }
 
-	private fun playCollection(index: Int = 0) {
-		lifecycleScope.launch {
-			val apiClient = StreamingClient()
+  private fun playCollection(index: Int = 0) {
+    lifecycleScope.launch {
+      val apiClient = StreamingClient()
 
-			when (mCollection) {
-				is Album -> {
-					when (val result = apiClient.playAlbum((mCollection as Album).id!!, index)) {
-						is ApiResult.Success -> {}
-						is ApiResult.Error -> {
-							Log.e(TAG, "Error playing albums: ${result.exception}")
-						}
-					}
-				}
+      when (mCollection) {
+        is Album -> {
+          when (val result = apiClient.playAlbum((mCollection as Album).id!!, index)) {
+            is ApiResult.Success -> {}
+            is ApiResult.Error -> {
+              Log.e(TAG, "Error playing albums: ${result.exception}")
+            }
+          }
+        }
 
-				is Mix -> {
-					when (val result = apiClient.playMix((mCollection as Mix).id!!, index)) {
-						is ApiResult.Success -> {}
-						is ApiResult.Error -> {
-							Log.e(TAG, "Error playing collection: ${result.exception}")
-						}
-					}
-				}
+        is Mix -> {
+          when (val result = apiClient.playMix((mCollection as Mix).id!!, index)) {
+            is ApiResult.Success -> {}
+            is ApiResult.Error -> {
+              Log.e(TAG, "Error playing collection: ${result.exception}")
+            }
+          }
+        }
 
-				is Playlist -> {
-					when (val result = apiClient.playPlaylist((mCollection as Playlist).uuid!!, index)) {
-						is ApiResult.Success -> {}
-						is ApiResult.Error -> {
-							Log.e(TAG, "Error playing collection: ${result.exception}")
-						}
-					}
-				}
-			}
-		}
+        is Playlist -> {
+          when (val result =
+            apiClient.playPlaylist((mCollection as Playlist).uuid!!, index)) {
+            is ApiResult.Success -> {}
+            is ApiResult.Error -> {
+              Log.e(TAG, "Error playing collection: ${result.exception}")
+            }
+          }
+        }
+      }
+    }
 
-	}
+  }
 
+  private fun queueCollection(position: EnqueuePosition) {
 
-	override fun onGenerated(palette: Palette?) {
+    // we need tracks
+    if (mCollection?.tracks == null) {
+      return
+    }
 
-		// get paletteColors
-		val paletteUtils = PaletteUtils(palette!!)
-		val actionsBgColor: Int = ColorUtils.setAlphaComponent(paletteUtils.getTitleBgColor(), ALPHA_VALUE)
-		val contentBgColor: Int = ColorUtils.setAlphaComponent(paletteUtils.getContentBgColor(), ALPHA_VALUE)
+    lifecycleScope.launch {
+      val apiClient = StreamingClient()
+      when (val result =
+        apiClient.enqueueTracks(mCollection!!.tracks!!.toTypedArray(), position)) {
+        is ApiResult.Success -> {}
+        is ApiResult.Error -> {
+          Log.e(TAG, "Error playing albums: ${result.exception}")
+        }
+      }
+    }
+  }
 
-		// background
-		mDetailsPresenter.setActionsBackgroundColor(actionsBgColor)
-		mDetailsPresenter.setBackgroundColor(contentBgColor)
-		mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
-	}
+  override fun onGenerated(palette: Palette?) {
+
+    // get paletteColors
+    val paletteUtils = PaletteUtils(palette!!)
+    val actionsBgColor: Int = ColorUtils.setAlphaComponent(paletteUtils.getTitleBgColor(), ALPHA_VALUE)
+    val contentBgColor: Int = ColorUtils.setAlphaComponent(paletteUtils.getContentBgColor(), ALPHA_VALUE)
+
+    // background
+    mDetailsPresenter.setActionsBackgroundColor(actionsBgColor)
+    mDetailsPresenter.setBackgroundColor(contentBgColor)
+    mAdapter.notifyArrayItemRangeChanged(0, mAdapter.size())
+  }
 
 
-	companion object {
-		private const val TAG = "VideoDetailsFragment"
+  companion object {
+    private const val TAG = "VideoDetailsFragment"
 
-		private const val ACTION_PLAY_NOW = 1L
-//		private const val ACTION_PLAY_NEXT = 2L
-//		private const val ACTION_QUEUE = 3L
-		private const val ACTION_GO_TO_ARTIST = 4L
+    private const val ACTION_PLAY_NOW = 1L
+    private const val ACTION_PLAY_NEXT = 2L
+    private const val ACTION_QUEUE = 3L
+    private const val ACTION_GO_TO_ARTIST = 4L
 
-		private const val DETAIL_THUMB_WIDTH = 274
-		private const val DETAIL_THUMB_HEIGHT = 274
+    private const val DETAIL_THUMB_WIDTH = 274
+    private const val DETAIL_THUMB_HEIGHT = 274
 
-		private const val ALPHA_VALUE = 232
+    private const val ALPHA_VALUE = 232
 
-	}
+  }
 
 }
--- app/src/main/java/fr/bonamy/tidalstreamer/collection/CollectionPresenter.kt
+++ app/src/main/java/fr/bonamy/tidalstreamer/collection/CollectionPresenter.kt
@@ -15,106 +15,113 @@ import fr.bonamy.tidalstreamer.models.Track
 import java.util.Locale
 
 interface OnTrackClickListener {
-	fun onTrackClick(track: Track)
-	fun onTrackLongClick(track: Track)
+  fun onTrackClick(track: Track)
+  fun onTrackLongClick(track: Track)
 }
 
-class DetailsPresenter(private val mCollection: Collection, private val mListener: OnTrackClickListener) : Presenter(){
+class DetailsPresenter(private val mCollection: Collection, private val mListener: OnTrackClickListener) : Presenter() {
 
-	private fun onCreateView(parent: ViewGroup): View {
-		return LayoutInflater.from(parent.context)
-			.inflate(R.layout.layout_details, parent, false)
-	}
+  private fun onCreateView(parent: ViewGroup): View {
+    return LayoutInflater.from(parent.context)
+      .inflate(R.layout.layout_details, parent, false)
+  }
 
-	override fun onCreateViewHolder(parent: ViewGroup?): Presenter.ViewHolder {
-		val view: View = onCreateView(parent!!)
-		val vh = ViewHolder(view)
-		vh.tracks.layoutManager = LinearLayoutManager(parent.context)
-		vh.tracks.adapter = TrackAdapter(mCollection, listOf(), mListener)
-		return vh
-	}
+  override fun onCreateViewHolder(parent: ViewGroup?): Presenter.ViewHolder {
+    val view: View = onCreateView(parent!!)
+    val vh = ViewHolder(view)
+    vh.tracks.layoutManager = LinearLayoutManager(parent.context)
+    vh.tracks.adapter = TrackAdapter(mCollection, listOf(), mListener)
+    return vh
+  }
 
-	@SuppressLint("SetTextI18n")
-	override fun onBindViewHolder(viewHolder: Presenter.ViewHolder?, item: Any?) {
-		val collection = item as Collection
-		val vh = viewHolder as ViewHolder
-		vh.title.text = collection.title()
-		vh.subtitle.text = collection.subtitle()
-		if (collection is Album && collection.releaseDate != null) {
-			val tokens = collection.releaseDate!!.split("-")
-			vh.releaseDate.text = "${tokens.get(1)}/${tokens.get(0)}"
-		}
-		(vh.tracks.adapter as TrackAdapter).updateData(collection.tracks ?: listOf())
-	}
+  @SuppressLint("SetTextI18n")
+  override fun onBindViewHolder(viewHolder: Presenter.ViewHolder?, item: Any?) {
+    val collection = item as Collection
+    val vh = viewHolder as ViewHolder
+    vh.title.text = collection.title()
+    vh.subtitle.text = collection.subtitle()
+    if (collection is Album && collection.releaseDate != null) {
+      val tokens = collection.releaseDate!!.split("-")
+      vh.releaseDate.text = tokens.get(0)
+    }
+    if (collection.tracks != null) {
+      val trackCount = collection.tracks!!.size
+      val trackCountText = vh.view.getResources().getQuantityString(R.plurals.track_count, trackCount, trackCount)
+      vh.trackCount.text = trackCountText
+    }
+    vh.trackCount.text = "${collection.tracks?.size?.toString()} tracks" ?: ""
+    (vh.tracks.adapter as TrackAdapter).updateData(collection.tracks ?: listOf())
+  }
 
-	override fun onUnbindViewHolder(viewHolder: Presenter.ViewHolder?) {
-	}
+  override fun onUnbindViewHolder(viewHolder: Presenter.ViewHolder?) {
+  }
 
-	class ViewHolder(view: View) : Presenter.ViewHolder(view) {
-		val title: TextView = view.findViewById<View>(R.id.details_title) as TextView
-		val subtitle: TextView =
-			view.findViewById<View>(R.id.details_subtitle) as TextView
-		var releaseDate: TextView = view.findViewById<View>(R.id.details_release_date) as TextView
-		val tracks: RecyclerView = view.findViewById<View>(R.id.details_tracks) as RecyclerView
-	}
+  class ViewHolder(view: View) : Presenter.ViewHolder(view) {
+    val title: TextView = view.findViewById<View>(R.id.details_title) as TextView
+    val subtitle: TextView =
+      view.findViewById<View>(R.id.details_subtitle) as TextView
+    var releaseDate: TextView = view.findViewById<View>(R.id.details_1) as TextView
+    var trackCount = view.findViewById<View>(R.id.details_2) as TextView
+    val tracks: RecyclerView = view.findViewById<View>(R.id.details_tracks) as RecyclerView
+  }
 
-	class TrackAdapter(private var mCollection: Collection, private var mList: List<Track>, private val listener: OnTrackClickListener) : RecyclerView.Adapter<TrackAdapter.ViewHolder>() {
+  class TrackAdapter(private var mCollection: Collection, private var mList: List<Track>, private val listener: OnTrackClickListener) : RecyclerView.Adapter<TrackAdapter.ViewHolder>() {
 
-		fun updateData(newList: List<Track>) {
-			mList = newList
-			notifyDataSetChanged()
-		}
+    fun updateData(newList: List<Track>) {
+      mList = newList
+      notifyDataSetChanged()
+    }
 
-		override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-			val view = LayoutInflater.from(parent.context)
-				.inflate(R.layout.item_track, parent, false)
-			return ViewHolder(view)
-		}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+      val view = LayoutInflater.from(parent.context)
+        .inflate(R.layout.item_track, parent, false)
+      return ViewHolder(view)
+    }
 
-		// binds the list items to a view
-		override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-			// basic
-			val track = mList[position]
-			holder.itemView.setOnClickListener {
-				listener.onTrackClick(track)
-			}
-			holder.itemView.setOnLongClickListener {
-				listener.onTrackLongClick(track)
-				true
-			}
+      // basic
+      val track = mList[position]
+      holder.itemView.setOnClickListener {
+        listener.onTrackClick(track)
+      }
+      holder.itemView.setOnLongClickListener {
+        listener.onTrackLongClick(track)
+        true
+      }
 
-			// index
-			if (mCollection is Album) {
-				holder.index.visibility = View.VISIBLE
-				holder.index.text = String.format(Locale.getDefault(), "%d.", track.trackNumber)
-			} else {
-				holder.index.visibility = View.GONE
-			}
+      // index
+      if (mCollection is Album) {
+        holder.index.visibility = View.VISIBLE
+        holder.index.text = String.format(Locale.getDefault(), "%d.", track.trackNumber)
+      } else {
+        holder.index.visibility = View.GONE
+      }
 
-			// title
-			if ((track.artist == null && track.artists != null) || track.index != null) {
-				holder.title.text = track.title + " - " + track.artists!!.map { it.name }.joinToString(", ")
-			} else {
-				holder.title.text = track.title
-			}
+      // title
+      if ((track.artist == null && track.artists != null) || track.index != null) {
+        holder.title.text = track.title + " - " + track.artists!!.map { it.name }.joinToString(", ")
+      } else {
+        holder.title.text = track.title
+      }
 
-			// duration
-			holder.duration.text = track.durationString()
+      // duration
+      holder.duration.text = track.durationString()
 
-		}
+    }
 
-		// return the number of the items in the list
-		override fun getItemCount(): Int {
-			return mList.size
-		}
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+      return mList.size
+    }
 
-		// Holds the views for adding it to image and text
-		class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-			val index: TextView = itemView.findViewById(R.id.index)
-			val title: TextView = itemView.findViewById(R.id.title)
-			val duration: TextView = itemView.findViewById(R.id.duration)
-		}
-	}
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+      val index: TextView = itemView.findViewById(R.id.index)
+      val title: TextView = itemView.findViewById(R.id.title)
+      val duration: TextView = itemView.findViewById(R.id.duration)
+    }
+  }
 
 }
