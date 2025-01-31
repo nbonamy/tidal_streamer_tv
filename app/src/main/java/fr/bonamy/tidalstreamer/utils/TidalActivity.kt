f030aa818088c1df539925aa857056d01f281f65
Nicolas Bonamy <nicolas@bonamy.fr>
2025-01-30 22:10:25 -0600

    auto playback

--- app/src/main/java/fr/bonamy/tidalstreamer/utils/TidalActivity.kt
+++ app/src/main/java/fr/bonamy/tidalstreamer/utils/TidalActivity.kt
@@ -2,6 +2,8 @@ package fr.bonamy.tidalstreamer.utils
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.KeyEvent
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
@@ -14,16 +16,17 @@ import kotlinx.coroutines.launch
 abstract class TidalActivity : FragmentActivity() {
 
   private lateinit var apiClient: StreamingClient
+  private val handler = Handler(Looper.getMainLooper())
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    if (savedInstanceState == null) {
+//    if (savedInstanceState == null) {
 //      if (hasMiniPlayback()) {
 //        supportFragmentManager.beginTransaction()
 //          .replace(R.id.playback_fragment, MiniPlaybackFragment())
 //          .commitNow()
 //      }
-    }
+//    }
     apiClient = StreamingClient()
   }
 
@@ -31,17 +34,29 @@ abstract class TidalActivity : FragmentActivity() {
     return true
   }
 
+  override fun onResume() {
+    super.onResume()
+    schedulePlaybackTask()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    cancelPlaybackTask()
+  }
+
   override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+
+    // reschedule playback task
+    schedulePlaybackTask()
 
     // playback
     if (keyCode == KeyEvent.KEYCODE_INFO || keyCode == KeyEvent.KEYCODE_I) {
-      val intent = Intent(this, PlaybackActivity::class.java)
-      startActivity(intent)
+      startPlaybackActivity()
       return true
     }
 
     // user collection
-    if (keyCode == KeyEvent.KEYCODE_LAST_CHANNEL || keyCode == KeyEvent.KEYCODE_INFO || keyCode == KeyEvent.KEYCODE_J) {
+    if (keyCode == KeyEvent.KEYCODE_LAST_CHANNEL || keyCode == KeyEvent.KEYCODE_J) {
       val intent = Intent(this, UserActivity::class.java)
       startActivity(intent)
       return true
@@ -107,6 +122,44 @@ abstract class TidalActivity : FragmentActivity() {
 
     // default
     return super.onKeyDown(keyCode, event)
+  }
+
+  private fun cancelPlaybackTask() {
+    handler.removeCallbacks(playbackTask)
+  }
+
+  private fun schedulePlaybackTask() {
+    cancelPlaybackTask()
+    handler.postDelayed(playbackTask, REFRESH_INTERVAL)
+  }
+
+  private val playbackTask = object : Runnable {
+    override fun run() {
+      lifecycleScope.launch {
+        when (val status = apiClient.status()) {
+          is ApiResult.Success -> {
+            if (status.data.state == "PLAYING") {
+              startPlaybackActivity()
+            }
+          }
+
+          is ApiResult.Error -> {
+          }
+        }
+      }
+
+      handler.postDelayed(this, REFRESH_INTERVAL)
+    }
+  }
+
+  private fun startPlaybackActivity() {
+    val intent = Intent(this, PlaybackActivity::class.java)
+    startActivity(intent)
+  }
+
+  companion object {
+    //private const val TAG = "PlaybackFragment"
+    private const val REFRESH_INTERVAL = 10000L
   }
 
 }
