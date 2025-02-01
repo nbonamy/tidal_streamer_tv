d4df81e47fa2641c191648d9dc29e45e62ef48fc
Nicolas Bonamy <nicolas@bonamy.fr>
2025-02-01 12:54:12 -0600

    stuff

--- app/src/main/java/fr/bonamy/tidalstreamer/models/Album.kt
+++ app/src/main/java/fr/bonamy/tidalstreamer/models/Album.kt
@@ -5,14 +5,17 @@ import android.graphics.Color
 data class Album(
   var id: String? = null,
   var title: String? = null,
+  var version: String? = null,
   var cover: String? = null,
   override var artist: Artist? = null,
   override var artists: List<Artist>? = null,
+  var duration: Int? = null,
   var vibrantColor: String? = null,
   var releaseDate: String? = null,
   var numberOfVolumes: Int = 0,
   var numberOfTracks: Int = 0,
   var popularity: Int? = null,
+  var audioQuality: String? = null,
 ) : Collection(), PlayedBy {
 
   override fun title(): String {
@@ -36,13 +39,25 @@ data class Album(
     return vibrantColor?.let { Color.parseColor(it) }
   }
 
+//  val quality: Int
+//    get() {
+//      return when (audioQuality) {
+//        "LOW" -> 1
+//        "HIGH" -> 2
+//        "LOSSLESS" -> 3
+//        "HI_RES" -> 4
+//        "HIRES_LOSSLESS" -> 5
+//        else -> 0
+//      }
+//    }
+
   override fun toString(): String {
     return "Album{" +
-        "id=" + id +
-        ", title='" + title + '\'' +
-        ", cover='" + cover + '\'' +
-        ", releaseDate='" + releaseDate + '\'' +
-        '}'
+      "id=" + id +
+      ", title='" + title + '\'' +
+      ", cover='" + cover + '\'' +
+      ", releaseDate='" + releaseDate + '\'' +
+      '}'
   }
 
 }
